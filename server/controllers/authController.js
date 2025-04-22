ac89c784b2164d9b246cd3fe3447cc1b027bcca1
Diti092005 <38325454791@mby.co.il>
2025-04-22 19:19:33 +0300

    After Doing the models and start the controllers

---
+++ server/controllers/authController.js
@@ -0,0 +1,35 @@
+const User = require("../models/User")
+const bcrypt = require('bcrypt')
+
+const login = async (req, res) => {
+    const {userid,password}=req.body
+    if(!userid || !password)
+        return res.status(400).json({message:"password and userid are required!!"})
+    const foundUser =await User.findOne(userid).lean()
+    if(!foundUser|| !foundUser.active)
+        return res.status(401).json({ message: 'Unauthorized' })
+    const match = await bcrypt.compare(password,foundUser.password)
+    if(!match)
+        return res.status(401).json({message:"Unauthorized"})
+    res.send("Logged in")
+ }
+const register = async (req, res) => {
+    const { userid, password, fullname, email, phone, street, numOfBulding, city, dateOfBirth, roles } = req.body
+    if (!fullname || !userid || !password) {// Confirm data
+        return res.status(400).json({ message: 'All fields are required' })
+    }
+    const duplicate = await User.findOne({userid}).lean()
+    if (duplicate) {
+        return res.status(409).json({ message: "Duplicate userid" })
+    }
+    const hashedPwd = await bcrypt.hash(password, 10)
+    const userObject = { fullname, email, userid, phone,email,address:{city,street,numOfBulding},dateOfBirth,roles, password: hashedPwd }
+    const user = await User.create(userObject)
+    if (user) { // Created
+        return res.status(201).json({
+            message: `New user ${user.userid} created` })
+    } else {
+        return res.status(400).json({ message: 'Invalid user received' })
+    }
+}
+module.exports = { login, register }
